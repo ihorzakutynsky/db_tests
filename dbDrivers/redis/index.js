ad570c433d8b7a96223ab05f17440e9f23801e3c
ivz-dev <zakutynsky@yahoo.com>
2019-02-04 10:15:42 +0200

    redis update

--- dbDrivers/redis/index.js
+++ dbDrivers/redis/index.js
@@ -1,7 +1,18 @@
 const redis = require("redis");
+const RedisClustr = require('redis-clustr');
 const _ = require("lodash");
 
-const client = redis.createClient();
+const client = new RedisClustr({
+    servers: [
+        {
+            host: '127.0.0.1',
+            port: '6379'
+        }
+    ],
+    createClient: function (port, host) {
+        return redis.createClient(port, host);
+    }
+});
 
 
 client.on("error", (err) => {
